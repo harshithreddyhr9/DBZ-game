46f7c36bcb4507813d8abf22717260ef9e72de43
Saumya Goyal <saumya.iis@gmail.com>
2018-11-21 23:48:08 -0800

    Added movement to enemies

--- DragonBall_Game/Buu.java
+++ DragonBall_Game/Buu.java
@@ -8,12 +8,24 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Buu extends Actor
 {
+    int count = 0;
     /**
      * Act - do whatever the Buu wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        //move(5);
+      count++;
+      if(count == 50){
+    if (getY() == 105){
+      setLocation(getX(), getY()+20);
+      count =0;}// "if not at target y-coordinate, move toward target y-coordinate"
+      else if (getY() > 105){
+      setLocation(getX(), getY()-40);
+      count =0;}// "if not at target y-coordinate, move toward target y-coordinate"
+      else if (getY() < 105){
+      setLocation(getX(), getY()+40);
+      count =0;}
+    }
     }    
 }
--- DragonBall_Game/Cell.java
+++ DragonBall_Game/Cell.java
@@ -8,12 +8,24 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Cell extends Actor
 {
+    int count = 0;
     /**
      * Act - do whatever the Cell wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
+             count++;
+      if(count == 50){
+    if (getY() == 235){
+      setLocation(getX(), getY()+20);
+      count =0;}// "if not at target y-coordinate, move toward target y-coordinate"
+      else if (getY() > 235){
+      setLocation(getX(), getY()-40);
+      count =0;}// "if not at target y-coordinate, move toward target y-coordinate"
+      else if (getY() < 235){
+      setLocation(getX(), getY()+40);
+      count =0;}
+    }
     }    
 }
